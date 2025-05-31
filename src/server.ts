d3ac09ae18caf4f75c44d3d55661454b31d5dcf5
smeysmey1509 <smeyhem01@gmail.com>
2025-05-31 16:24:12 +0700

    change port

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ mongoose
   )
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(process.env.PORT || 5000, () => {
+    app.listen(process.env.PORT || 5003, () => {
       console.log(`Server is running on port ${process.env.PORT || 5000}`);
     });
   })
--- test.js
+++
@@ -1,5 +0,0 @@
-const str = "APPLE";
-let reverse = ''
-for (let i = str.length; i >= 0 ; i--) {
-    console.log(reverse += str[i]);
-}

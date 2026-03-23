feef9bad71311ae309ecd520b92a58a2f629df28
smeysmey1509 <smeyhem01@gmail.com>
2026-03-23 09:09:27 +0700

    latest

--- dist/main/utils/redisClient.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const redis_1 = require("redis");
-const redisClient = (0, redis_1.createClient)();
-redisClient.on("error", err => { console.error("❌ Redis Client Error", err); });
-redisClient.connect().then(err => {
-    console.log("✅ Redis connected");
-});
-exports.default = redisClient;
